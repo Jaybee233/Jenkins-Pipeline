4c15943d596a778423c562a8c92a5bb3825292d4
jaybee233 <neast-z@foxmail.com>
2018-03-18 15:26:17 +0800

    Jaybee
    添加参考URL

--- Jenkins_comm.groovy
+++ Jenkins_comm.groovy
@@ -1,4 +1,6 @@
 #创建一个可以读取文本并且修改后能够提交的Pipeline
+#https://support.cloudbees.com/hc/en-us/articles/226554067-Pipeline-How-to-add-an-input-step-with-timeout-that-continues-if-timeout-is-reached-using-a-default-value
+
 node {
    echo "JOB_DISPLAY_URL:$JOB_DISPLAY_URL"
    def tmppath='/Users/jiangxiaoxiong/Documents/GitHub/display-url-api-plugin/a.tmp'
